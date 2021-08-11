85262532fefeac6b28d3fb8ecbe6027903518264
Udayraj <udayraj.m@bizamica.com>
2021-08-11 20:18:30 +0530

    Add checkSlide function to slide in images during scroll[DONE]

--- script.js
+++ script.js
@@ -17,8 +17,19 @@
 
   function checkSlide(e){
     sliderImages.forEach(sliderImage => {
-        const slideInAt = (window.scrollY + window.innerHeight);
-        console.log(slideInAt);
+        // half way through the image
+        const slideInAt = (window.scrollY + window.innerHeight)- (sliderImage.height)/2;
+        // bottom of the image
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+
+        if(isHalfShown && isNotScrolledPast){
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
     });
   }
 
